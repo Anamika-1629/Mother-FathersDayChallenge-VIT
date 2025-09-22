14f186196909ef7e83539dd5da9592575946d060
Anamika-1629 <anamikarai835@gmail.com>
2025-09-22 20:18:54 +0530

    day 23

--- Day23/SearchInsertPosition.java
+++ Day23/SearchInsertPosition.java
@@ -1,4 +1,4 @@
-class Solution {
+class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
         int l = 0, r = nums.length - 1;
         while (l <= r) {
@@ -15,7 +15,7 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        SearchInsertPosition sol = new SearchInsertPosition();
         int[] nums = {1, 3, 5, 6};
         int target = 5;
         System.out.println(sol.searchInsert(nums, target)); 
